77a1b8f102317b69f27d9271ccca4a2b3af2035b
13626646811 <1216951702@qq.com>
2025-06-10 20:15:50 +0800

    modify

---
+++ chapter03/src/main/java/com/clf/Main.java
@@ -0,0 +1,16 @@
+package com.clf;
+
+/**
+ * ClassName: ${NAME}
+ * Package:
+ * Description:
+ *
+ * @Author clf
+ * @Create 2024/12/24 12:13
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
