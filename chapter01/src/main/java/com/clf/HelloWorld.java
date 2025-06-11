a7bf198063c1f9f5544fae2b516eb819afd29873
13626646811 <1216951702@qq.com>
2025-06-11 13:57:48 +0800

    modify

--- chapter01/src/main/java/com/clf/HelloWorld.java
+++ chapter01/src/main/java/com/clf/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.clf;
 
+
 class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("hello,world!!你好，中国！");
--- chapter01/src/main/java/com/clf/Main.java
+++ chapter01/src/main/java/com/clf/Main.java
@@ -1,5 +1,7 @@
 package com.clf;
 
+import java.util.ArrayList;
+
 /**
  * ClassName: ${NAME}
  * Package: com.clf
@@ -10,7 +12,13 @@ package com.clf;
  * @Version 1.0
  */
 public class Main {
+    public  static ArrayList<Integer>  arrayList = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("Hello, World!!!");
+//        while(true){
+//            int i = 0;
+//            arrayList.add(new Integer(i++));
+//        }
     }
+
 }
